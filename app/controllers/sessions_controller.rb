e69cb649a7ef28898a4638fd67fed32fc5b8d69f
giuseppe Vetri <gvetri18@gmail.com>
2016-08-21 21:58:53 -0400

    Arreglando errores en el login y registro

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -45,9 +45,10 @@ class SessionsController < Devise::SessionsController
 
       }
       format.html{
-        signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-        set_flash_message! :notice, :signed_out if signed_out
-        yield if block_given?
+        super
+        # signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+        # set_flash_message! :notice, :signed_out if signed_out
+        # yield if block_given?
       }
     end
   end
